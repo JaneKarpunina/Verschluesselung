8c697ac4e1facbade095596b78bf65dbd5654e17
evgeniyakarpunina <janekarpunina@gmail.com>
2017-11-18 21:23:27 +0100

    comments

--- src/pis/hue1/Codec.java
+++ src/pis/hue1/Codec.java
@@ -1,12 +1,40 @@
 package pis.hue1;
 
+
+/**
+ * Codec Interface is designed to provide a common protocol for objects
+ * that are used for coding and decoding Strings.
+ * It is written for coding and decoding string only and is using string as a key.
+ * If a key is not valid then IllegalArgumentException will be thrown
+ */
 public interface Codec {
 
+    /**
+     * The method for coding a string using a specified key
+     *
+     * @param klartext text to be coded
+     * @return String - result of the coding
+     */
     String kodiere(String klartext);
 
+    /**
+     * The method for decoding a string using a specified key
+     *
+     * @param geheimtext text to be decoded
+     * @return String - result of the decoding
+     */
     String dekodiere(String geheimtext);
 
+    /**
+     * @return String - key word which is used for coding
+     */
     String gibLosung();
 
+    /**
+     * sets key word to be used for coding
+     *
+     * @param schluessel is a key word
+     * @throws IllegalArgumentException when the key word is not allowed
+     */
     void setzeLosung(String schluessel) throws IllegalArgumentException; // bei ungeeignetem Schlüssel!
 }
